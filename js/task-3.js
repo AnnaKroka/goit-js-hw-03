c440a6698bfd7d3e9d4f10632bd7d9e80992564d
Anna Kroka <avkroka.broker@gmail.com>
2024-06-27 17:58:54 +0300

    make tasks 1-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,5 @@
+//-------------- TASK 3-1--------------------
+
 // function getElementWidth(content, padding, border) {
 //     return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
 // }
@@ -5,18 +7,39 @@
 // console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 // console.log(getElementWidth("200px", "0px", "0px")); // 200
 
-function checkForSpam(message) {
-    if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) {
-        return true;
-    } else {
-        return false;
+//-------------- TASK 3-3--------------------
+
+// function checkForSpam(message) {
+//     if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+//-------------- TASK 3-3--------------------
+
+function filterArray(numbers, value) {
+    let arrayNumber = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            arrayNumber.push(numbers[i]);
+        } 
+        
     }
+    return arrayNumber;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
